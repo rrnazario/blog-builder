07a067f137f678e505b71c1edf0b5281a307dc86
Rogerson Nazário <rrnazario@gmail.com>
2024-02-25 14:50:51 +0100

    chore: new summary + wpp link

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,18 @@ function App() {
       alignItems: 'center'
     }}>
       <PresentationCard
-        buttonCaption="Saiba mais"
+        buttonCaption="Fale conosco"
         pictureUrl="/pres3.png"
         title="Salve galera!"
-        summary="labore 
-      suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-      modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
-      totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
-      quasi aliquam eligendi, placeat qui corporis!"
+        summary={[
+          "Se você caiu aqui de paraquedas nós somos Gabi e Éro, um casal brasileiro que decidiu construir uma vida juntos na ensolarada Málaga, na Espanha.",
+          "Compartilhamos o nosso dia a dia, dicas sobre a vida na Espanha, curiosidades locais e tudo o que envolve a imigração e os trâmites necessários para viver legalmente no país.",
+          "Além de compartilhar nossas experiências, também oferecemos assessoria para quem deseja imigrar para a Espanha, tornando esse processo tão burocrático mais fácil e descomplicado. Desde a obtenção de visto até a busca por moradia, estamos aqui para te ajudar em cada passo do caminho.",
+          "Se você está pensando em embarcar nessa aventura de viver na Espanha, que tal contar com a nossa experiência e expertise para tornar essa transição mais tranquila e segura? Entre em contato conosco e vamos juntos fazer desse sonho uma realidade!"]}
         style={{
           maxWidth: '1200px'
         }}
+        onClick={() => window.open("https://wa.me/634462597?text=Ol%C3%A1%2C%20vi%20o%20seu%20blog%20%22Di%C3%A1rio%20de%20Rol%C3%AA%22%20e%20gostaria%20de%20conhecer%20mais%20sobre%20o%20trabalho%20de%20voc%C3%AAs%21", "_blank")}
       />
       <br />
       <h2>Sigam nossas redes sociais</h2>
--- src/components/PresentationCard/index.tsx
+++ src/components/PresentationCard/index.tsx
@@ -6,30 +6,24 @@ interface PresentationCardProps {
     pictureUrl?: string,
     title?: string,
     subtitle?: string,
-    summary?: string,
+    summary?: string[],
     buttonCaption?: string,
     onClick?: (e: any) => void,
     style?: CSSProperties
 }
 
 export default function PresentationCard(props: PresentationCardProps) {
-    return <Styled.Container onClick={props.onClick} style={props.style} >
+    return <Styled.Container style={props.style} >
         <Styled.LeftPanel>
             <Styled.Picture src={props.pictureUrl} />
         </Styled.LeftPanel>
         <Styled.RightPanel>
             <h2>{props.title}</h2>
-            <br />
-            {props.subtitle && <>                
+            {props.subtitle && <>
                 <h4>{props.subtitle}</h4>
                 <br />
             </>}
-            <p>{props.summary}</p>
-            <br />
-            <p>{props.summary}</p>
-            <br />
-            <p>{props.summary}</p>
-            <br />
+            {props.summary && props.summary.map((value, index) => <><p key={index}>{value}</p></>)}
             <RoundedButton caption={props.buttonCaption ?? 'Saiba mais'} style={{
                 textAlign: 'center',
                 maxWidth: '200px',
@@ -37,7 +31,9 @@ export default function PresentationCard(props: PresentationCardProps) {
                 border: '2px solid var(--text-secondary)',
                 backgroundColor: 'transparent',
                 color: 'black'
-            }} />
+            }}
+                onClick={props.onClick}
+            />
         </Styled.RightPanel>
 
     </Styled.Container>
