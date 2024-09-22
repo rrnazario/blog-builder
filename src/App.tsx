404de9fbc8506d5f589aa9cea2b5581d3ec888fb
Rogerson Nazário <rrnazario@gmail.com>
2024-09-22 20:53:44 +0200

    ateneia

--- src/App.tsx
+++ src/App.tsx
@@ -161,6 +161,30 @@ function App() {
           onClick={() => navigator(pageRoutes.FORMULARIO)}
         />
 
+<PresentationCard
+          hideButton
+          id="sobre"
+          key={123}
+          pictureRound={'100px'}
+          pictureWidth={'200px'}
+          pictureHeight={'200px'}
+          buttonCaption="+ Assessoria"
+          pictureUrl="/ateneia.jpeg"
+          title="Atenéia"
+          summary={[
+            "A consultoria da Gaby e do Éro foi essencial para eu decidir em qual cidade iniciar minha aventura na Espanha: Málaga. ",
+            "Pude tirar dúvidas e ouvir as experiências que eles possuem sobre moradia, trabalho e vida social. ",
+            "Me senti mais segura para imigrar SOZINHA a partir da conversa que tive com eles. ",
+            "Além de tudo, a Gaby me ajudou a encontrar o quarto que eu alugo e vivo hoje (muito obrigada, Gabizinha). ",
+            "Na hora de mudar de país, existem muitas dúvidas, inseguranças, medos e ansiedade. ",
+            "Acredito que a consultoria pode desbloquear uma boa parte desses sentimentos e nos ajudar a fazer uma transição mentalmente mais tranquila."]}
+          style={{
+            maxWidth: '700px',
+            backgroundColor: '#BDE8FA'
+          }}
+          onClick={() => navigator(pageRoutes.FORMULARIO)}
+        />
+
       </div>
 
       {/*  */}
